6b5aa2b436b93d5d9db05c1bfa4550bb48927ded
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-10-08 13:44:36 +0200

    wip tokamdataset

--- solution/submission.py
+++ solution/submission.py
@@ -1,6 +1,26 @@
+from pathlib import Path
+from typing import Callable, Optional, Union
+
 import torch
-from torchvision.dataset import BoxDataset
+from torchvision.datasets.vision import VisionDataset
 from torchvision.models.detection import fasterrcnn_resnet50_fpn
+
+
+class TokamDataset(VisionDataset):
+    def __init__(
+        self,
+        root: Union[str, Path] = None,  # type: ignore[assignment]
+        transforms: Optional[Callable] = None,
+        transform: Optional[Callable] = None,
+        target_transform: Optional[Callable] = None,
+    ) -> None:
+        super().__init__(
+            self,
+            root=root,
+            transforms=transforms,
+            transform=transform,
+            target_transform=target_transform,
+        )
 
 
 def make_dataset(training_dir):
@@ -13,7 +33,7 @@ def make_dataset(training_dir):
 
 def train_model(training_dir):
     train_data = make_dataset(training_dir)
-    train_dataloader = torch.dataset.DataLoader(train_data, batch_size=4, ...)
+    train_dataloader = torch.dataset.DataLoader(train_data, batch_size=4)
 
     model = fasterrcnn_resnet50_fpn()
     model.train()
