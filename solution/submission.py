537af3a84192df2bf1d799d2ae2b7baf510b4ee3
Thomas Moreau <thomas.moreau.2010@gmail.com>
2025-11-26 22:48:29 +0100

    FIX running on GPU with actual predictions (#4)

--- solution/submission.py
+++ solution/submission.py
@@ -11,24 +11,34 @@ def collate_fn(batch: torch.Tensor) -> torch.Tensor:
 def train_model(training_dir):
     train_dataset = TokamDataset(training_dir)
     train_dataloader = torch.utils.data.DataLoader(
-        train_dataset, batch_size=1, collate_fn=collate_fn
+        train_dataset, batch_size=2, collate_fn=collate_fn, shuffle=True
     )
 
+    if torch.cuda.is_available():
+        print("Using GPU")
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+
     model = fasterrcnn_resnet50_fpn()
+    model.to(device)
     model.train()
 
-    optimizer = torch.optim.SGD(model.parameters())
+    optimizer = torch.optim.AdamW(model.parameters())
 
-    max_epochs = 1
+    max_epochs = 15
 
-    for _ in range(max_epochs):
+    for i in range(max_epochs):
+        print(f"Epoch {i+1}/{max_epochs}")
         for images, targets in train_dataloader:
+            images = [im.to(device) for im in images]
+            targets = [
+                {k: v.to(device) if isinstance(v, torch.Tensor) else v
+                 for k, v in t.items()} for t in targets
+            ]
             optimizer.zero_grad()
             loss_dict = model(images, targets)
             full_loss = sum(loss for loss in loss_dict.values())
             full_loss.backward()
             optimizer.step()
-            break
 
-    model.eval()
+    model.eval().to("cpu")
     return model
